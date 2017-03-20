a4bcc4c83bcb989216b9374a8a476fba3a3d030e
Andrei Bastos <andreibastos@outlook.com>
2017-03-19 22:02:59 -0300

    print duty
    imprimir duty a cada um segundo

--- speed-temperature.py
+++ speed-temperature.py
@@ -1,1 +1,46 @@
-#coding: utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from subprocess import call
+import time
+
+PWM_FREQUENCY = 100  #Hertz
+GPIO_FAN = 5 #pin number
+
+#configurando GPIO
+# import RPi.GPIO as IO
+# IO.setmode (IO.BCM)
+# GPIO.setup(GPIO_FAN ,GPIO.OUT)
+# pwm=GPIO.PWM(GPIO_FAN,100)     #Configura para 100 Hz
+# pwm.start(100)
+
+max_temperatura = 0; 
+min_temperatura = 0;
+
+while 1:
+		
+
+	#ler temperatura
+	temperatura = call(["/opt/vc/bin/vcgencmd", "measure_temp"]).replace("temp=","").replace("'C","");
+
+	#salva em algum lugar pra ver historico
+
+
+	#verifica se a temperatura atual é a maior registrada
+	if temperatura > max_temperatura:
+		max_temperatura = temperatura
+
+	diff_temperatura = max_temperatura - min_temperatura
+
+	duty = 0
+	if diff_temperatura!=0
+		duty = temperatura/(diff_temperatura*1.2)
+
+	print duty
+
+	time.spleep(1);
+
+
+
+
+
